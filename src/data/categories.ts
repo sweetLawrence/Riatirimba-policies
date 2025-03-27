e8b94b8d939aef460c4f1b7ebc4a84b1d2c14bcb
Lawrence <tsungulawrence@gmail.com>
2025-03-27 19:12:00 +0300

    code

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,9 +1,10 @@
 export const categories = [
+  { name: 'Full Board Committees Overview', path: 'fullboard' },
   { name: 'Risk & Audit Compliance Committee', path: 'audit' },
   {
     name: 'Finance, Operation and Infrastructure Development Committee',
     path: 'finance'
   },
   { name: 'Education, Training and HR Committee', path: 'academics' },
-  { name: 'Full Board Committees Overview', path: 'fullboard' }
+  
 ]
