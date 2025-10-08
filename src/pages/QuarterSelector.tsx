88e7b8397cfd2eaa3e15dcfa33134c8e680150c9
Lawrence <tsungulawrence@gmail.com>
2025-10-08 19:52:08 +0300

    code

--- src/pages/QuarterSelector.tsx
+++ src/pages/QuarterSelector.tsx
@@ -45,28 +45,27 @@
 //   )
 // }
 
-
-
-
-
 import { Link } from 'react-router-dom'
 import { BsFolderFill } from 'react-icons/bs'
 import { Image } from '@mantine/core'
 import LOGO from '../assets/logo.png'
 
 export default function QuarterSelector () {
-  const quarters = ['2024-2025.Q3', '2024-2025.Q4']
+  const quarters = ['2024-2025.Q3', '2024-2025.Q4', '2025-2026.Q1']
   const displayLabels: Record<string, string> = {
     '2024-2025.Q3': 'The 3RD_Q_2024-2025 Full _Board and Cmmtt_Min & Docs',
-    '2024-2025.Q4': 'The 4TH_Q_2024-2025 Full _Board and Cmmtt_Min & Docs'
+    '2024-2025.Q4': 'The 4TH_Q_2024-2025 Full _Board and Cmmtt_Min & Docs',
+    '2025-2026.Q1': 'The 1ST_Q_2025-2026 Full _Board and Cmmtt_Min & Docs'
   }
 
   return (
-    <div className="bdy overflow-hidden bg-gray-50 min-h-screen flex flex-col">
+    <div className='bdy overflow-hidden bg-gray-50 min-h-screen flex flex-col'>
       {/* Navbar */}
       <div className='bg-white shadow p-4 flex items-center gap-4'>
         <Image src={LOGO} alt='Logo' className='w-[40px] h-[40px]' />
-        <h1 className='text-xl font-semibold text-blue-900'>Riatirimba Docs and Policies</h1>
+        <h1 className='text-xl font-semibold text-blue-900'>
+          Riatirimba Docs and Policies
+        </h1>
       </div>
 
       {/* Main content */}
